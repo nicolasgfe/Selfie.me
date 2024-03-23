b78fa231e881cfb386a446216af9d1dcf74767c9
HelitonCalgaroto <whelyton_calgarotto@hotmail.com>
2024-03-23 09:44:58 -0300

    Criação do model usuarios

---
+++ backend/src/models/usuarios/usuarios.ts
@@ -0,0 +1,11 @@
+
+export class Usuarios {
+   public id_usuario: number;
+   public nome: string;
+   public sobrenome: string;
+   public email: string;
+   public status: boolean;
+   public criadoEm: Date;
+   public atualizadoEm: Date;
+   public senha: string;
+}
