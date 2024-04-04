71c35d37d75b3220dce66c9f7dbd398f462c7879
Nicolas G Fernandes <nicolas.fernadnes@toglab.co>
2024-04-04 19:58:30 -0300

    PSME-36: criando metodo do repository para conseguir buscar uma empresa.

--- backend/src/service/empresa/index.ts
+++ backend/src/service/empresa/index.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmpresaDto } from 'src/dto/empresa';
 import { Empresa } from 'src/entity/empresa';
 import { EmpresaRepository } from 'src/repository/empresa';
@@ -11,13 +11,13 @@ export class EmpresaService {
     return this.empresaRepository.findAll();
   }
 
-  // async findById(id: number): Promise<Plano> {
-  //   const user = await this.planosRepository.findById(id);
-  //   if (!user) {
-  //     throw new NotFoundException('User not found');
-  //   }
-  //   return user;
-  // }
+  async findById(id: number): Promise<Empresa> {
+    const empresa = await this.empresaRepository.findById(id);
+    if (!empresa) {
+      throw new NotFoundException('empresa not found');
+    }
+    return empresa;
+  }
 
   async create(createEmpresaDto: CreateEmpresaDto): Promise<Empresa> {
     return this.empresaRepository.create(createEmpresaDto);
