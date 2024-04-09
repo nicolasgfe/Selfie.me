8592e88b0b9217e0ce0497b15941b5ffe8164bdf
Guilherme De Souza <souzadegui@gmail.com>
2024-04-08 21:22:00 -0300

    HomePage

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,13 @@
+import cart from './cart.svg'
+import download from './dowload.svg'
+import hearth from './hearth.svg'
+import logo from './logo.svg'
+import user from './user.svg'
+
+export {
+    cart,
+    download,
+    hearth,
+    logo,
+    user,
+}
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "../pages/Home";
+import { Home } from '../pages/Home';
 
 export function Router() {
     return (
@@ -8,3 +8,4 @@ export function Router() {
         </Routes>
     )
 }
+
