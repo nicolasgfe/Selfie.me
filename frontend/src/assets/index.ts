1156e60ba5fcbdb37d9415d8e0643c817354ee30
JhonatanRizzi <104238615+jhonafer@users.noreply.github.com>
2024-04-17 02:43:18 -0300

    adicionado imagens na tela de conta

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -11,6 +11,18 @@ import homebackground from './images/homebackground.jpg'
 import OceanRocks from './images/OceanRocks.jpg'
 import BgYourCOunt from './images/BgYourCount.jpg'
 import perfilYour from './images/perfilYour.png'
+import bixoveio from './images/bixoveio.png'
+import camiao from './images/camiao.png'
+import macaco from './images/macaco.png'
+import murrodospreso from './images/murrodospreso.png'
+import torredebabel from './images/torredebabel.png'
+import logosonora from './images/logosonora.jpg'
+import logoexpoflor from './images/logoexpoflor.jpg'
+import mia from './images/mia.png'
+import music from './images/music.png'
+import onibus from './images/onibus.jpg'
+
+
 
 export {
     cart,
@@ -26,4 +38,14 @@ export {
     OceanRocks,
     BgYourCOunt,
     perfilYour,
+    bixoveio,
+    camiao,
+    macaco,
+    murrodospreso,
+    torredebabel,
+    logosonora,
+    logoexpoflor,
+    mia,
+    music,
+    onibus,
 }
