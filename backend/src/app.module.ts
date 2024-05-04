d048c9a35d784192f040e3012947262f3f84b193
Gmod9K <pablonofre@hotmail.com>
2024-05-04 10:15:41 -0300

    removido dados pessoais do projeto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -29,8 +29,8 @@ require('dotenv/config');
       type: 'postgres',
       host: process.env.DB_HOST,
       port: 5432,
-      username: "postgres",
-      password: "idessedi",
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       entities: [
         Plano,
